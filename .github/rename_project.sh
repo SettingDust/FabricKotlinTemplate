2b1ae9f948bc67136ed9fd95e995d327e1f23fb7
SettingDust <settingdust@gmail.com>
2023-11-17 00:26:18 +0800

    ci: try self writing template

---
+++ .github/rename_project.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+while getopts a:n:d: flag
+do
+    case "${flag}" in
+        a) author=${OPTARG};;
+        n) name=${OPTARG};;
+        d) description=${OPTARG};;
+    esac
+done
+
+name_snake=$(echo "$name" | sed -r -e 's/^([A-Z]+)([A-Z])/\L\1\E\2/' -e 's/^[A-Z]/\l&/' -e 's/[A-Z]+$/-\L&/' -e 's/([A-Z]+)([A-Z])/-\L\1\E\2/g' -e 's/[A-Z]/-\l&/g')
+
+original_author="SettingDust"
+original_name="FabricKotlinTemplate"
+original_name_snake="fabric-kotlin-template"
+original_url="https://github.com/SettingDust/FabricKotlinTemplate"
+original_description="An example mod"
+original_group="settingdust.template"
+group="$(echo "$author" | tr '[:upper:]' '[:lower:]').$(echo "$name" | sed 's/ //g' | tr '[:upper:]' '[:lower:]')"
+group_path="$(echo "$group" | tr '.' '/')"
+url="https://github.com/$author/$name"
+
+echo "Author: $author";
+echo "Project Name: $name";
+echo "Project Name Snake: $name_snake";
+echo "Description: $description";
+echo "Group: $group";
+echo "Group Path: $group_path";
+echo "Url: $url";
+
+echo "Renaming project..."
+
+# for filename in $(find . -name "*.*")
+for filename in $(git ls-files)
+do
+  echo "sed -i s|$original_author|$author|g $filename"
+    sed -i "s|$original_author|$author|g $filename"
+  echo "sed -i s|$original_name|$name|g $filename"
+    sed -i "s|$original_name|$name|g" "$filename"
+  echo "sed -i s|$original_name_snake|$name_snake|g $filename"
+    sed -i "s|$original_name_snake|$name_snake|g" "$filename"
+  echo "sed -i s|$original_url|$url|g $filename"
+    sed -i "s|$original_url|$url|g" "$filename"
+  echo "sed -i s|$original_description|$description|g $filename"
+    sed -i "s|$original_description|$description|g" "$filename"
+  echo "sed -i s|$original_group|$group|g $filename"
+    sed -i "s|$original_group|$group|g" "$filename"
+    echo "Mapped $filename"
+done
+
+mv src/client/java/settingdust/template "src/client/java/$group_path"
+mv src/client/kotlin/settingdust/template "src/client/kotlin/$group_path"
+mv src/client/resources/assets/fabric-kotlin-template "src/client/resources/assets/$name_snake"
+mv src/client/resources/fabric-kotlin-template.client.mixins.json "src/client/resources/$name_snake.client.mixins.json"
+mv src/main/java/settingdust/template "src/main/java/$group_path"
+mv src/main/kotlin/settingdust/template "src/main/kotlin/$group_path"
+mv src/main/resources/fabric-kotlin-template.mixins.json "src/main/resources/$name_snake.mixins.json"
+# This command runs only once on GHA!
+rm -rf .github/template.yml
